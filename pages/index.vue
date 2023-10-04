0043dc2ef07c3ee7930906fa98ee3bfa8e557ee9
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-10-04 18:45:39 -0500

    fix:Se modifica login segun lo requerido

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
         <div class="forms-container">
             <div class="signin-signup">
                 <form action="#" class="sign-in-form">
+                    <h2 class="title">Bienvenido</h2>
                     <h2 class="title">Ingresa aquí</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
@@ -12,7 +13,7 @@
                         <i class="fas fa-lock"></i>
                         <input type="password" placeholder="Contraseña" />
                     </div>
-                    <input type="submit" value="Login" class="btn solid" />
+                    <input type="submit" value="Ingresar" class="btn solid" />
                     <!-- <p class="social-text">Or Sign in with social platforms</p>
                     <div class="social-media">
                         <a href="#" class="social-icon">
@@ -29,7 +30,7 @@
                         </a>
                     </div> -->
                 </form>
-                <form action="#" class="sign-up-form">
+                <!-- <form action="#" class="sign-up-form">
                     <h2 class="title">Regístrate</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
@@ -43,7 +44,7 @@
                         <i class="fas fa-lock"></i>
                         <input type="password" placeholder="Contraseña" />
                     </div>
-                    <input type="submit" class="btn" value="Sign up" />
+                    <input type="submit" class="btn" value="Sign up" /> -->
                     <!-- <p class="social-text">Or Sign up with social platforms</p>
                     <div class="social-media">
                         <a href="#" class="social-icon">
@@ -59,25 +60,25 @@
                             <i class="fab fa-linkedin-in"></i>
                         </a>
                     </div> -->
-                </form>
+                <!-- </form> -->
             </div>
         </div>
 
         <div class="panels-container">
             <div class="panel left-panel">
                 <div class="content">
-                    <h3>Eres nuevo ?</h3>
+                    <h3>Lorem ipsum</h3>
                     <p>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
                         ex ratione. Aliquid!
                     </p>
-                    <button class="btn transparent" id="sign-up-btn" v-on:click="toggleSignupMode">
+                    <!-- <button class="btn transparent" id="sign-up-btn" v-on:click="toggleSignupMode">
                         Regístrate
-                    </button>
+                    </button> -->
                 </div>
                 <img src="./images/log.svg" class="image" alt="" />
             </div>
-            <div class="panel right-panel">
+            <!-- <div class="panel right-panel">
                 <div class="content">
                     <h3>Ya eres miembro ?</h3>
                     <p>
@@ -87,9 +88,9 @@
                     <button class="btn transparent" id="sign-in-btn" v-on:click="toggleSignupMode">
                         Ingresar
                     </button>
-                </div>
-                <img src="./images/register.svg" class="image" alt="" />
-            </div>
+                </div> -->
+                <!-- <img src="./images/register.svg" class="image" alt="" /> -->
+            <!-- </div> -->
         </div>
     </div>
 </template>
