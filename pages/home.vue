fe4ff3a70bfeaab9921a4639b82d6baa2c05a951
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-10-04 22:27:58 -0500

    feat:validaciones login, alertas y apifake

---
+++ pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>HOME</h1>
+</template>
+<script setup>
+import Swal from "sweetalert2";
+//Alerta de inicio de sesion existoso
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Inicio de sesión exitoso'
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,20 @@
 <template>
-    <div class="container" v-bind:class="{ 'sign-up-mode': signupMode }">
+    <div class="container">
         <div class="forms-container">
             <div class="signin-signup">
-                <form action="#" class="sign-in-form">
+                <form action="#" class="sign-in-form" @submit.prevent="handleSubmit">
                     <h2 class="title">Bienvenido</h2>
                     <h2 class="title">Ingresa aquí</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Nombre de usuario" />
+                        <input v-model="email" placeholder="Correo electrónico" />
                     </div>
                     <div class="input-field">
                         <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Contraseña" />
+                        <input v-model="password" type="password" placeholder="Contraseña" />
                     </div>
                     <input type="submit" value="Ingresar" class="btn solid" />
-                    <!-- <p class="social-text">Or Sign in with social platforms</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </div> -->
                 </form>
-                <!-- <form action="#" class="sign-up-form">
-                    <h2 class="title">Regístrate</h2>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Nombre de usuario" />
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-envelope"></i>
-                        <input type="email" placeholder="Correo" />
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Contraseña" />
-                    </div>
-                    <input type="submit" class="btn" value="Sign up" /> -->
-                    <!-- <p class="social-text">Or Sign up with social platforms</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </div> -->
-                <!-- </form> -->
             </div>
         </div>
 
@@ -72,35 +26,74 @@
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
                         ex ratione. Aliquid!
                     </p>
-                    <!-- <button class="btn transparent" id="sign-up-btn" v-on:click="toggleSignupMode">
-                        Regístrate
-                    </button> -->
                 </div>
                 <img src="./images/log.svg" class="image" alt="" />
             </div>
-            <!-- <div class="panel right-panel">
-                <div class="content">
-                    <h3>Ya eres miembro ?</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                        laboriosam ad deleniti.
-                    </p>
-                    <button class="btn transparent" id="sign-in-btn" v-on:click="toggleSignupMode">
-                        Ingresar
-                    </button>
-                </div> -->
-                <!-- <img src="./images/register.svg" class="image" alt="" /> -->
-            <!-- </div> -->
         </div>
     </div>
 </template>
 
 <style  src="./styles.index.css"></style>
-
 <script setup>
-let signupMode = ref(false)
+import axios from "axios";
+import Swal from "sweetalert2";
 
-function toggleSignupMode() {
-    signupMode.value = !signupMode.value;
+const router = useRouter();
+const email = ref('');
+const password = ref('');
+const errorMessage = ref('');
+const users = ref([])
+
+const handleSubmit = async () => {
+    // Validación del correo electrónico
+    if (!email.value || !/^\S+@\S+\.\S+$/.test(email.value)) {
+        errorMessage.value = 'Por favor, ingresa un correo electrónico válido.';
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: errorMessage.value,
+        })
+        return;
+    }
+
+    // Validación de la contraseña
+    if (!password.value) {
+        errorMessage.value = 'Por favor, ingresa tu contraseña.';
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: errorMessage.value,
+        })
+        return;
+    }
+
+    errorMessage.value = '';
+    await login();
+};
+
+const getUsers = async () => {
+    const url = "http://localhost:3001/users"
+    const response = await axios.get(url)
+    users.value = response.data
+}
+
+const login = async () => {
+    await getUsers()
+    console.log(users.value);
+    const foundUser = users.value.find(
+        user =>
+            user.email === email.value && user.password === password.value
+    );
+    if (foundUser) {
+        console.log('Inicio de sesión exitoso para el usuario:', foundUser);
+        router.push('/home');
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oh no...',
+            text: 'Credenciales incorrectas, inténtalo de nuevo!',
+
+        })
+    }
 }
 </script>
