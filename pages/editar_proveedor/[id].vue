e32bca2be8f5e4e175a8166f05ed48d699fbf66a
mamnuu <kioko1908@gmail.com>
2023-10-19 00:44:34 -0500

    Fix: validar campos proveedor

--- pages/editar_proveedor/[id].vue
+++ pages/editar_proveedor/[id].vue
@@ -1,37 +1,74 @@
 <template>
   <div class="register-container">
     <div class="form-container">
-      <v-card class="mx-auto pa-6 pb-2" elevation="20" max-width="448" rounded="lg">
+      <v-card
+        class="mx-auto pa-6 pb-2"
+        elevation="20"
+        max-width="448"
+        rounded="lg"
+      >
         <v-card-title style="text-align: center">Proveedores</v-card-title>
-        <v-card-subtitle style="text-align: center">Formulario para actualizar proveedores</v-card-subtitle>
+        <v-card-subtitle style="text-align: center"
+          >Formulario para actualizar proveedores</v-card-subtitle
+        >
         <div class="text-subtitle-1 text-medium-emphasis">Nombre</div>
 
-        <v-text-field v-model="foundSupplier.nombre" density="compact" placeholder="Nombre" prepend-inner-icon="mdi-account-outline"
-          variant="underlined" :rules="Rules"></v-text-field>
+        <v-text-field
+          v-model="foundSupplier.nombre"
+          density="compact"
+          placeholder="Nombre"
+          prepend-inner-icon="mdi-account-outline"
+          variant="underlined"
+          :rules="Rules"
+        ></v-text-field>
 
         <div class="text-subtitle-1 text-medium-emphasis">
           Correo electrónico
         </div>
 
-        <v-text-field v-model="foundSupplier.correo" density="compact" placeholder="Correo electrónico"
-          prepend-inner-icon="mdi-email-outline" variant="underlined" :rules="Rules"></v-text-field>
+        <v-text-field
+          v-model="foundSupplier.correo"
+          density="compact"
+          placeholder="Correo electrónico"
+          prepend-inner-icon="mdi-email-outline"
+          variant="underlined"
+          :rules="Rules"
+        ></v-text-field>
 
         <div class="text-subtitle-1 text-medium-emphasis">
           Producto que provee
         </div>
 
-        <v-text-field v-model="foundSupplier.producto" density="compact" placeholder="Producto" prepend-inner-icon="mdi-cart-outline"
-          variant="underlined" :rules="Rules"></v-text-field>
+        <v-text-field
+          v-model="foundSupplier.producto"
+          density="compact"
+          placeholder="Producto"
+          prepend-inner-icon="mdi-cart-outline"
+          variant="underlined"
+          :rules="Rules"
+        ></v-text-field>
 
         <div class="text-subtitle-1 text-medium-emphasis">
           Número de contacto
         </div>
 
-        <v-text-field v-model="foundSupplier.contacto" density="compact" placeholder="Número de contacto"
-          prepend-inner-icon="mdi-card-account-phone-outline" variant="underlined"
-          :rules="[Rules, contactoRules].flat()"></v-text-field>
+        <v-text-field
+          v-model="foundSupplier.contacto"
+          density="compact"
+          placeholder="Número de contacto"
+          prepend-inner-icon="mdi-card-account-phone-outline"
+          variant="underlined"
+          :rules="[Rules, contactoRules].flat()"
+        ></v-text-field>
 
-        <v-btn block class="mb-8" color="#5995fd" size="large" variant="outlined" @click="update">Actualizar
+        <v-btn
+          block
+          class="mb-8"
+          color="#5995fd"
+          size="large"
+          variant="outlined"
+          @click="validatefields"
+          >Actualizar
         </v-btn>
       </v-card>
     </div>
@@ -49,13 +86,17 @@ let foundSupplier = ref({});
 const errorMessage = ref("");
 
 const Rules = [(v) => !!v || "Este campo es requerido"];
-const contactoRules = [v => (/^[0-9]+|[()\.]+/.test(v)) || 'Solo se permiten números'];
+const contactoRules = [
+  (v) => /^[0-9]+|[()\.]+/.test(v) || "Solo se permiten números",
+];
 
 onBeforeMount(async () => {
   try {
     await getSuppliers();
     console.log(suppliers.value);
-    foundSupplier.value = suppliers.value.find((supplier) => supplier.id == supplierId);
+    foundSupplier.value = suppliers.value.find(
+      (supplier) => supplier.id == supplierId
+    );
     if (!foundSupplier) {
       throw new Error("Proveedor no encontrado");
     }
@@ -70,7 +111,45 @@ const getSuppliers = async () => {
   const response = await axios.get(url);
   suppliers.value = response.data;
 };
+const validatefields = async () => {
+  if (
+    foundSupplier.value.nombre == "" ||
+    foundSupplier.value.correo == "" ||
+    foundSupplier.value.producto == "" ||
+    foundSupplier.value.contacto == ""
+  ) {
+    errorMessage.value = "Por favor, ingresa todos los campos.";
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: errorMessage.value,
+    });
+    return;
+  }
+  // Validación de solo números
+  if (!/[0-9-]+/.test(foundSupplier.value.contacto)) {
+    errorMessage.value = "Solo se permiten números";
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: errorMessage.value,
+    });
+    return;
+  }
 
+  // Validación correo válido
+  if (!foundSupplier.value.correo || !/^\S+@\S+\.\S+$/.test(foundSupplier.value.correo)) {
+    console.log("Por favor, ingresa un correo electrónico válido.");
+    errorMessage.value = "Por favor, ingresa un correo electrónico válido.";
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: errorMessage.value,
+    });
+    return;
+  }
+  await update();
+};
 const update = async () => {
   try {
     await updateSuppliers();
@@ -87,7 +166,7 @@ const update = async () => {
   } catch (error) {
     console.log(errorMessage);
   }
-}
+};
 
 const updateSuppliers = async () => {
   const url = `http://localhost:3001/suppliers/${foundSupplier.value.id}`;
@@ -98,8 +177,7 @@ const updateSuppliers = async () => {
 
 definePageMeta({
   layout: "blank",
-})
-
+});
 </script>
 
 <style scoped>
@@ -122,7 +200,7 @@ definePageMeta({
   width: 100%;
   background-color: #fff;
   height: 100%;
-  overflow:visible;
+  overflow: visible;
   margin-top: 3%;
 }
 
